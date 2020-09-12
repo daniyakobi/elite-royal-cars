ff286eed1126f25d36a4832de274d9781519837c
daniyakobi <nadeev.danil2016@yandex.ru>
2020-09-12 14:01:14 +0300

    Create: Advantages

--- js/script.js
+++ js/script.js
@@ -5,7 +5,6 @@ $(document).ready(function() {
         const main = $('.main').height();
         const navbar = $('.navbar').height();
         const navbarTop = $('.navbar-top').height();
-        console.log(main, navbar, navbarTop, scrollTop);
 
         if (scrollTop => navbar * 2) {
             $('.navbar').css({
@@ -18,4 +17,16 @@ $(document).ready(function() {
             });
         };
     });
+
+    var mySwiper = new Swiper('.reviews__slider-container', {
+        // Optional parameters
+        loop: true,
+      
+        // Navigation arrows
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+      });
+
 });
